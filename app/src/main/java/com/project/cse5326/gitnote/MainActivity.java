a248aac3ad2344c46bb5913c6a8fc16b2b1ea512
JoannaSiFang <sifang95@gmail.com>
2017-11-05 11:46:30 -0500

    Implemented showing all note list in the main page

--- app/src/main/java/com/project/cse5326/gitnote/MainActivity.java
+++ app/src/main/java/com/project/cse5326/gitnote/MainActivity.java
@@ -1,22 +1,20 @@
 package com.project.cse5326.gitnote;
 
-import android.app.Fragment;
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         setUpDrawer();
+        showNoteList();
     }
 
     @Override
@@ -118,4 +117,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void showNoteList(){
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+            if(fragment == null){
+                fragment = new NoteListFragment();
+                fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+            }
+    }
+
 }
